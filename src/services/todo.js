da86ef1e048164739d77f79bafbd8b95e8e3cd4d
Paulo Hernane <paulohernane10@gmail.com>
2021-11-19 11:06:14 -0300

    refatorando os metodos que consomem a API

--- src/services/todo.js
+++ src/services/todo.js
@@ -1,19 +1,11 @@
 import client from '../providers/todo-api';
 
-const post = body => {
-  client.post('/', body);
-};
+const post = body => client.post('/', body);
 
-const getAll = () => {
-  return client.get('/');
-};
+const getAll = () => client.get('/');
 
-const update = ({ id, ...body }) => {
-  client.put(`/${id}`, body);
-};
+const update = ({ id, ...body }) => client.put(`/${id}`, body);
 
-const del = id => {
-  client.delete(`/${id}`);
-};
+const del = id => client.delete(`/${id}`);
 
 export { post, getAll, update, del };
