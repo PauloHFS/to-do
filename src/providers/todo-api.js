8af62bd9ac14b08691349af202f79984eaa0e9aa
Paulo Hernane <paulohernane10@gmail.com>
2021-12-26 19:28:51 -0300

    change api url

--- src/providers/todo-api.js
+++ src/providers/todo-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: 'https://to-do-api-pauloh.herokuapp.com/',
+  baseURL: 'http://localhost:3333',
 });
 
 export default client;
