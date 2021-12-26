b0906f1faa86de004f1f40de9eadf76029ca3ce4
Paulo Hernane <paulohernane10@gmail.com>
2021-12-26 19:30:25 -0300

    fix(form): acertando modos adição/edição do form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,19 +1,23 @@
+import { Fragment, useEffect, useState } from 'react';
+
 import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 
 import { post, update } from '../../services/todo';
-import { useEffect } from 'react';
 
-const Form = ({ type, todoData, method }) => {
+const Form = ({ todoData, method }) => {
   let navigate = useNavigate();
+
   const { handleSubmit, register, setValue } = useForm();
+
+  const [todo, setTodo] = useState({});
 
   useEffect(() => {
     if (todoData) {
-      setValue([
-        { title: todoData.title },
-        { description: todoData.description },
-      ]);
+      ['title', 'description'].forEach(field =>
+        setValue(field, todoData[field])
+      );
+      setTodo(todoData);
     }
   }, [todoData]);
 
@@ -35,7 +39,8 @@ const Form = ({ type, todoData, method }) => {
   };
 
   return (
-    <>
+    <Fragment>
+      <h3>{todoData ? 'Edit mode' : 'Add mode'}</h3>
       <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="title">Title</label>
         <input type="text" {...register('title')} />
@@ -43,7 +48,7 @@ const Form = ({ type, todoData, method }) => {
         <input type="text" {...register('description')} />
         <button>Enviar</button>
       </form>
-    </>
+    </Fragment>
   );
 };
 
